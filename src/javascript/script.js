7bae1d4634766e5ea9f5a39abd3daf5c23c9df81
L7kkj <lucasrssholanda@gmail.com>
2025-06-08 19:45:22 -0300

    redirecionamento do login para home

---
+++ src/javascript/script.js
@@ -0,0 +1,32 @@
+function salvarlogin(event) {
+  event.preventDefault()
+
+  const emailInput = document.getElementById("usuário").value.trim();
+  const senhaInput = document.getElementById("senha").value;
+
+  const usuarios = JSON.parse(localStorage.getItem("usuarios") || "[]");
+
+  const usuarioValido = usuarios.find(user => user.email === emailInput && user.senha === senhaInput);
+
+  let mensagem = document.getElementById("mensagemLogin");
+  if (!mensagem) {
+    mensagem = document.createElement("div");
+    mensagem.id = "mensagemLogin";
+    mensagem.style.marginTop = "10px";
+    mensagem.style.fontWeight = "bold";
+    document.querySelector("form").appendChild(mensagem);
+  }
+
+  if (usuarioValido) {
+    mensagem.style.color = "green";
+    mensagem.textContent = "Login realizado com sucesso! Redirecionando...";
+
+
+    setTimeout(() => {
+      window.location.href = "../pages/Home.html"
+    }, 1000);
+  } else {
+    mensagem.style.color = "red";
+    mensagem.textContent = "E-mail ou senha incorretos.";
+}
+}
